44ee5e72df80b4634b336cf30ee7c594552460ca
G.viswakanth <viswakanthreddy06@gmail.com>
2017-08-11 00:24:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,8 +8,9 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/article',function(req,res){
-    res.send('Article one is requested and will be served here');
+app.get('/article-one',function(req,res){
+      res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+    
 });
 app.get('/article-two',function(req,res){
     res.send('Article two is requested and will be served here');
