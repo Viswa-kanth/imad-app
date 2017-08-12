a8a5c7bec241ebfd979fcc5e47d6548af8653a9b
G.viswakanth <viswakanthreddy06@gmail.com>
2017-08-12 23:15:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,58 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articleone={
+    title:'Article one i viswakanth',
+    heading:'Article one',
+    date:'sep14,2017',
+    content:
+            ` <p>
+                 This is the content of my first article. This is the content of my first article. This is the content of my first article.
+                  This is the content of my first article. This is the content of my first article.
+            </p>
+         <p>
+            This is the content of my first article. This is the content of my first article. This is the content of my first article.
+          This is the content of my first article. This is the content of my first article.
+          </p>
+          <p>
+          This is the content of my first article. This is the content of my first article. This is the content of my first article This is the content of my first article. This is the content of my first article.
+    </p>`
+};
+function createtemplate(data){
+    var title=data.title;
+    var data=data.date;
+    var heading=data.heading;
+    var content=data.content
+var htmltemplate=`
+<html>
+<head>
+    <title>
+        $ {title}
+    </title>
+         <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    <body>
+        <div class = "container">
+        <div>
+            <a href="/">Home</a>
+        </div>
+        <hr/>
+            <h3>
+            $ {heading}
+            </h3>
+            <div>
+                $ {date}
+            </div>
+            <div>
+         $ {content}
+        </div>
+    </div>
+    </body>
+    </html>
+    return htmltemplate;
+    `;
+}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
