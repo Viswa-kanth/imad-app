c17e799f95d4b1b15ea5461dde6b4d61b83ce17d
G.viswakanth <viswakanthreddy06@gmail.com>
2017-08-12 23:24:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -61,8 +61,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-one',function(req,res){
-      res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
-    
+      res.send(createtemplate(articleone));
 });
 app.get('/article-two',function(req,res){
       res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
