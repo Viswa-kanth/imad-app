3d88127ebf6110a664b024a25b335743fb11b7cb
G.viswakanth <viswakanthreddy06@gmail.com>
2017-08-14 00:19:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,7 +3,13 @@ console.log('Loaded!');
 var element = document.getElementById('main-text');
 element.innerHTML = 'New value';
 //move the image
-var img =document.getElementById('madi');
-img.onclick = function () {
-    img.style.marginleft = '100px';
-    };
+var img = document.getElementById('madi');
+var marginleft =0;
+function moveRight() {
+    marginLeft = marginLeft+10;
+    img.style.marginLeft = marginLeft+ 'px';
+}
+img.onclick() = function () {
+    var interval = setInterval(moveRight,100);
+    }
+
