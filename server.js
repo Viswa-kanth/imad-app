3ee790e11d24f6de492ff5531dcc7884ac369a9f
G.viswakanth <viswakanthreddy06@gmail.com>
2017-08-16 07:40:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -75,10 +75,10 @@ names.push(name);//concatenate to overall list of names
 res.send(JSON.stringify(names));
 });
 
-app.get('/:articleName',function(req,res){
+app.get('/:articleName',function(req,res) {
     var articleName = req.params.articleName;
     res.send(createTemplate(articles[articleName]));
-    });
+    }
     app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
