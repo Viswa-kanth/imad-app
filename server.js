15602d8897454ee1e510e4a64ec5fb835201434e
G.viswakanth <viswakanthreddy06@gmail.com>
2017-08-16 07:44:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -65,14 +65,6 @@ app.get('/counter',function(req,res) {
     counter = counter + 1;
     res.send(counter.toString());
 });
-
-app.get('/:articleName',function(req,res) {
-    var articleName = req.params.articleName;
-    res.send(createTemplate(articles[articleName]));
-    });
-    app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
-});
 var names = [];
 app.get('/submit-name/:name',function(req,res){ //URL:/submit-name?name=xxxxx
     //get the name from request
