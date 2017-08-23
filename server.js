330e422808e57df8501b0435b0d40eb8f148dcd9
G.viswakanth <viswakanthreddy06@gmail.com>
2017-08-23 19:24:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -97,7 +97,7 @@ pool.query('SELECT * FROM test',function(req,res){
     if(err){
         res.status(500).send(err.toString());
         }else{
-            res.send(JSON.stringify(result));
+            res.send(JSON.stringify(result.rows));
         }
 });
 });
