634acc10fbfee09ede86ffeff232b13c69cfb2e7
G.viswakanth <viswakanthreddy06@gmail.com>
2017-08-23 19:19:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var pool = require('pg').pool;
+var config = {
+    user: 'viswakanthreddy06',
+    database: 'viswakanthreddy06',
+    host: 'db.imad.hasura-app.io',
+    port: '5432',
+    password: process.env.DB-PASSWORD
+};
 
 var app = express();
 app.use(morgan('combined'));
@@ -80,6 +88,18 @@ var htmlTemplate=`
 }
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+var pool = new pool(config);
+app.get('/test-db',function(req,res){
+    //make a select request
+    //return a response with results
+pool.query('SELECT * FROM test',function(req,res){
+    if(err){
+        res.status(500).send(err.toString());
+        }else{
+            res.send(JSON.stringify(result));
+        }
+});
 });
 var counter = 0;
 app.get('/counter',function(req,res) {
