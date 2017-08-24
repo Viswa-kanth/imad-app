97bfd72aaf5d0117dc54b4fea596864cd7890829
G.viswakanth <viswakanthreddy06@gmail.com>
2017-08-24 07:26:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var config = {
     database: 'viswakanthreddy06',
     host: 'db.imad.hasura-app.io',
     port: '5432',
-    password: process.env.DB-KANTH
+    password: process.env.db-viswakanthreddy06-2511
 };
 var app = express();
 app.use(morgan('combined'));
