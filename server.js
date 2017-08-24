af9a2c55d63dd99f51da8f2d6341ee2e4ebc0de1
G.viswakanth <viswakanthreddy06@gmail.com>
2017-08-24 23:15:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -81,10 +81,9 @@ var htmlTemplate= `
         </div>
     </div>
     </body>
-    </html>
+    </html>`;
     return htmlTemplate;
-    `;
-}
+    }
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
