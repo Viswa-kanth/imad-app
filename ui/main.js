6c58d3b10bf7a33954cd2f0ca06060761f5ec2b5
G.viswakanth <viswakanthreddy06@gmail.com>
2017-08-29 01:33:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,43 +1,28 @@
-//counter code
-var counter = 0;  //submit name
+//submit username/password to login
 var submit = document.getElementById('submit-btn');
-    submit.onclick = function () {
-    var button = document.getElementById('counter');
-button.onclick = function (){
-    //create a request object
+submit.onclick = function () {
+    //create a new request
     var request = new XMLHttpRequest();
-    //capture response and store it in a variable
+    //capture the response and store it ina variable
     request.onreadystatechange = function () {
-        if(request.readystate === XMLHttpRequest.DONE){
-           //take some action
-           if(request.status === 200){
-               var names = request.responseText;
-               names = JSON.parase(names);
-        var list = '';
-        for(var i = 0; i<names.length; i++){
-        list += '<li>'+ names[i]+ '</li>';
-        }
-        var ul = document.getElementById('namelist');
-        ul.innerHTML = list;
-          
-           }
-        }
-        };
-        //not done yet
-        //make a request
-          var nameInput = document.getElementById('name');
-    var name = nameInput.value;
-        request.open('post','http://viswakanthreddy06.imad.hasura-app.io/submit-name?name='+name,true);
-        request.send(null);
-    };    
-        //make a request to server and send name
-        //capture a list of names and render it as a list
-        var names = ['name1','name2','name3','name4'];
-        var list = '';
-        for(var i = 0; i<names.length; i++){
-        list += '<li>'+ names[i]+ '</li>';
-        }
-        var ul = document.getElementById('namelist');
-        ul.innerHTML = list;
+    if (request.readyState === XMLHttpRequestDONE) {
+    //take some action
+    if (request.status === 200) {
+    alert('logged in successfully');
+    } else if (request.status === 403) {
+        alert('Username/password is incorrect');
+    } else if (request.status === 500) {
+    alert('Something went wrong on the server');
+    }
+    }
     };
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+    console.log(username);
+    conole.log(password);
+    request.open('post','http://viswakanthreddy06.imad.hasura-app.io/login',true);
+    request.setRequestHeader('Content-Type', 'application/json');
+    request.send(JSON.stringify({username: username,password: password}));
+    };    
+
      
