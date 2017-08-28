8d7b103d560f4d844bd8cc5b349a02ad00cec2fa
G.viswakanth <viswakanthreddy06@gmail.com>
2017-08-29 01:52:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -20,7 +20,7 @@ submit.onclick = function () {
     var password = document.getElementById('password').value;
     console.log(username);
     console.log(password);
-    request.open('post','http://viswakanthreddy06.imad.hasura-app.io/login',true);
+    request.open('POST', 'http://viswakanthreddy06.imad.hasura-app.io/login',true);
     request.setRequestHeader('Content-Type', 'application/json');
     request.send(JSON.stringify({username: username, password: password}));
     };    
